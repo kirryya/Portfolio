1c4f733cd69d5813ed9ed879a943999394b4a95f
kirryya <kir1987@inbox.ru>
2022-04-14 12:42:29 +0300

    skill

--- src/skills/Skills.tsx
+++ src/skills/Skills.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import style from "./Skills.module.css"
 import styleContainer from "../common/styles/Container.module.css"
+import Skill from "./skill/Skill";
 
 const Skills = () => {
+
     return (
         <div className={style.skillsBlock}>
             <div className={`${styleContainer.container} ${style.skillsContainer} `}>
                 <h2 className={style.title}>Skills</h2>
                 <div className={style.skills}>
-
+                    <Skill title={"JS"} description={"JSJSJSJSJSJSJSJSJSJSJSJS"}/>
+                    <Skill title={"CSS"} description={"CSSCSSCSSCSSCSS CSSCSSCSSCSSCSS"}/>
+                    <Skill title={"React"} description={"ReactReactReactReactReactReact"}/>
                 </div>
             </div>
         </div>
