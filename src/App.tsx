e3813f495a484345e2af4bccc34e3a8793e90676
kirryya <kir1987@inbox.ru>
2022-04-19 23:14:59 +0300

    adaptive

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Header from "./header/Header";
 import Main from "./main/Main";
 import Skills from "./skills/Skills";
 import Projects from "./projects/Projects";
-import Contact from "./contacts/Contact";
+import Contact from "./contact/Contact";
 import Footer from "./footer/Footer";
 
 function App() {
