dcf819262ed877fc95f16d9bdd107829e101efc2
kirryya <kir1987@inbox.ru>
2022-07-05 14:50:20 +0300

    contact form

--- src/contact/Contact.tsx
+++ src/contact/Contact.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {ChangeEvent, useState} from 'react';
 import style from "./Contacts.module.scss";
 import styleContainer from "../common/styles/Container.module.scss";
 import {Title} from "../common/components/title/Title";
@@ -12,6 +12,31 @@ export const Contact = () => {
 
     const Fade = require("react-reveal/Fade");
 
+    const [yourName, setYourName] = useState<string>("")
+    const [email, setEmail] = useState<string>("")
+    const [message, setMessage] = useState<string>("")
+    const [send, setSend] = useState<boolean>(false)
+
+    const onChangeNameHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setYourName(e.currentTarget.value)
+    }
+    const onChangeEmailHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setEmail(e.currentTarget.value)
+    }
+    const onChangeMessageHandler = (e: ChangeEvent<HTMLTextAreaElement>) => {
+        setMessage(e.currentTarget.value)
+    }
+    const onClickSendHandler = () => {
+        setYourName("")
+        setMessage("")
+        setEmail("")
+        setSend(true)
+        setTimeout(() => {
+            setSend(false)
+        }, 3000)
+    }
+
+
     return (
         <div className={style.contactsBlock} style={font} id={"contact"}>
             <Fade right>
@@ -19,11 +44,19 @@ export const Contact = () => {
                     <Title title={"Contact"}/>
                     <div className={style.addForm}>
                         <form className={style.form}>
-                            <input placeholder={"Your name"} className={style.nameForm} onChange={() => {}}/>
-                            <input placeholder={"Your email"} className={style.emailForm} onChange={() => {}}/>
-                            <textarea placeholder={"Your message"} className={style.messageForm} onChange={() => {}}/>
+                            <input placeholder={"Your name"} value={yourName} className={style.nameForm}
+                                   onChange={onChangeNameHandler}/>
+                            <input placeholder={"Your email"} value={email} className={style.emailForm}
+                                   onChange={onChangeEmailHandler}/>
+                            <textarea placeholder={"Your message"} value={message} className={style.messageForm}
+                                      onChange={onChangeMessageHandler}/>
                         </form>
-                        <button className={style.button}>Send</button>
+                        {send ?
+                            <div className={style.message}>
+                                <span>Your message has been send</span>
+                                <span>Thank you!</span>
+                            </div>
+                            : <button className={style.button} onClick={onClickSendHandler}>Send</button>}
                     </div>
                 </div>
             </Fade>
